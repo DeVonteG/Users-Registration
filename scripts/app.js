2d9a656319f5b0449984f8b8124071120577cdfb
DeVonte G <dagray92@hotmail.com>
2022-06-06 05:40:35 -0400

    Registration
    Updated classes,  added more authentication

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,14 @@
-function user(userName,password){
-    this.userName=userName;
-    this.passWord=password;
-}
 
-function login(){
-    let userName = $("#txtUserName").val();
-    let passWord = $("#txtPassword").val();
 
-    // console.log(userName,passWord);
-    let currentUser = new user(userName,passWord);
-    console.log(currentUser)
-    $('input').val("");
-}
+// function login(){
+//     let userName = $("#txtUserName").val();
+//     let passWord = $("#txtPassword").val();
+
+//     // console.log(userName,passWord);
+//     let currentUser = new user(userName,passWord);
+//     console.log(currentUser)
+//     $('input').val("");
+// }
 
 
 
@@ -28,6 +25,35 @@ function User(email,password,fname,lname,age,address,phone,payment){
     
 
 } 
+function isValid(user){
+    // return false when the user is not valid
+    // return true when user is valid
+    let valid=true;
+    // add validation for email,pass,fname,lname
+    if(user.email.length==0){
+        valid=false;
+        $("#txtEmail").addClass("input-error");
+        // alert("Please add an email");
+    }
+
+    if(user.password=="" || user.password.length <=6){
+        valid=false;
+        $("#txtPassword").addClass("input-error");
+        alert("Password must be at least 7 characters");
+    }
+    if(user.firstName.length==0){
+        valid=false;
+        $("#txtFirstName").addClass("input-error");
+        // alert("Enter First Name");
+    }
+    if( user.lastName.length==0){
+        valid=false;
+        $("#txtLastName").addClass("input-error");
+
+    }
+    return valid;
+} 
+
 // register function
 function register(){
     let userName = $("#txtEmail").val();
@@ -43,16 +69,30 @@ function register(){
 
     // creating the obj
     let newUser = new User(userName,userPass,userFirstName,userLastName,userAge,userAddress,userPhone,paymentMethod);
-    // clear the inputs
-    $('input').val("");
-    console.log(newUser);
+
+    
+    if(isValid(newUser)==true){
+        // console.log(newUser);
+        saveUser(newUser); //this function is in the storeManager
+        // clear the inputs
+        $('input').val("");
+    }
+    
+    
 }
 
 function init(){
-    // console.log("Login");
-    $("#btnLogin").click(login);
-    // console.log("Register");
+    // // console.log("Login");
+    // $("#btnLogin").click(login);
+    console.log("Register");
     $("#btnRegister").click(register);
+    $("#txtLastName").keypress(function(e){
+        if(e.key=="Enter"){
+            register();
+        }
+        console.log(e.key);
+    });
+
 }
 
 window.onload=init;
